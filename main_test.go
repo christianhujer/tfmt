40fc4a16b8d77abfb4ec522fd8c83d3cac937a9c
Christian Hujer <Christian.Hujer@nelkinda.com>
2018-11-12 23:55:41 +0530

    Improve tests.

--- main_test.go
+++ main_test.go
@@ -1,66 +1,60 @@
 package tfmt
 
 import (
-	"fmt"
-	"github.com/christianhujer/interceptor"
+	"github.com/christianhujer/assert"
+	"github.com/christianhujer/intercept"
 	"strings"
 	"testing"
 )
 
-func AssertEquals(t *testing.T, expected interface{}, actual interface{}) {
-	if expected != actual {
-		t.Error(fmt.Sprintf("Expected: %s\nActual: %s\n", expected, actual))
-	}
-}
-
 func TestTsprintf(t *testing.T) {
 	expected := "foo bar"
 	actual := Tsprintf("%{p1}s %{p2}s", map[string]interface{}{"p1": "foo", "p2": "bar"})
-	AssertEquals(t, expected, actual)
+	assert.Equals(t, expected, actual)
 }
 
 func TestTsprintfDoesNotFormatAgain(t *testing.T) {
 	expected := "%s bar"
 	actual := Tsprintf("%{p1}s %{p2}s", map[string]interface{}{"p1": "%s", "p2": "bar"})
-	AssertEquals(t, expected, actual)
+	assert.Equals(t, expected, actual)
 }
 
 func TestTfprintf(t *testing.T) {
 	expected := "foo bar"
 	builder := strings.Builder{}
 	Tfprintf(&builder, "%{p1}s %{p2}s", map[string]interface{}{"p1": "foo", "p2": "bar"})
-	AssertEquals(t, expected, builder.String())
+	assert.Equals(t, expected, builder.String())
 }
 
 func TestTfprintfDoesNotFormatAgain(t *testing.T) {
 	expected := "%s bar"
 	builder := strings.Builder{}
 	Tfprintf(&builder, "%{p1}s %{p2}s", map[string]interface{}{"p1": "%s", "p2": "bar"})
-	AssertEquals(t, expected, builder.String())
+	assert.Equals(t, expected, builder.String())
 }
 
 func TestTprintf(t *testing.T) {
 	expected := "foo bar"
-	stdout, stderr, err := interceptor.InterceptStrings(func() { Tprintf("%{p1}s %{p2}s", map[string]interface{}{"p1": "foo", "p2": "bar"}) })
+	stdout, stderr, err := intercept.Strings(func() { Tprintf("%{p1}s %{p2}s", map[string]interface{}{"p1": "foo", "p2": "bar"}) })
 	if err != nil {
 		t.Error(err)
 	}
-	AssertEquals(t, expected, *stdout)
-	AssertEquals(t, "", *stderr)
+	assert.Equals(t, expected, *stdout)
+	assert.Equals(t, "", *stderr)
 }
 
 func TestTprintfDoesNotFormatAgain(t *testing.T) {
 	expected := "%s bar"
-	stdout, stderr, err := interceptor.InterceptStrings(func() { Tprintf("%{p1}s %{p2}s", map[string]interface{}{"p1": "%s", "p2": "bar"}) })
+	stdout, stderr, err := intercept.Strings(func() { Tprintf("%{p1}s %{p2}s", map[string]interface{}{"p1": "%s", "p2": "bar"}) })
 	if err != nil {
 		t.Error(err)
 	}
-	AssertEquals(t, expected, *stdout)
-	AssertEquals(t, "", *stderr)
+	assert.Equals(t, expected, *stdout)
+	assert.Equals(t, "", *stderr)
 }
 
 //func TestTsprintfDoesNotReplaceRecursively(t *testing.T) {
 //	expected := "%{p2}s bar"
 //	actual := Tsprintf("%{p1}s %{p2}s", map[string]interface{}{"p1": "%{p2}s", "p2": "bar"})
-//	AssertEquals(t, expected, actual)
+//	assert.Equals(t, expected, actual)
 //}
