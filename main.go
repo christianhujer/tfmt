21fae5f72403e8dc1bff0fe512caf296a7554aa9
Christian Hujer <Christian.Hujer@nelkinda.com>
2018-11-12 12:21:26 +0530

    Extract common function.

--- main.go
+++ main.go
@@ -6,23 +6,21 @@ import (
 	"strings"
 )
 
-func Tprintf(format string, params map[string]interface{}) (n int, err error) {
+func formatString(format string, params map[string]interface{}) string {
 	for key, value := range params {
 		format = strings.Replace(format, "%{"+key+"}s", fmt.Sprintf("%s", value), -1)
 	}
-	return fmt.Printf(format)
+	return format
+}
+
+func Tprintf(format string, params map[string]interface{}) (n int, err error) {
+	return fmt.Printf(formatString(format, params))
 }
 
 func Tfprintf(w io.Writer, format string, params map[string]interface{}) (n int, err error) {
-	for key, value := range params {
-		format = strings.Replace(format, "%{"+key+"}s", fmt.Sprintf("%s", value), -1)
-	}
-	return fmt.Fprintf(w, format)
+	return fmt.Fprintf(w, formatString(format, params))
 }
 
 func Tsprintf(format string, params map[string]interface{}) string {
-	for key, value := range params {
-		format = strings.Replace(format, "%{"+key+"}s", fmt.Sprintf("%s", value), -1)
-	}
-	return fmt.Sprintf(format)
+	return fmt.Sprintf(formatString(format, params))
 }
